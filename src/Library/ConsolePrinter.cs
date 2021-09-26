cb783b9072e36599771a60b21e5e3a33c94c3b52
FrancoDest <88690071+FrancoDest@users.noreply.github.com>
2021-09-26 20:59:23 -0300

    Agregamos comentarios explicativos

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -1,3 +1,10 @@
+//---------------------------------------------------------------------------------
+// Utilizamos los principios de polimorfismo para cambiar la clase "AllInOnePrinter" 
+// a una interfaz con dos sub clases.
+// Borramos y volvimos a crear el archivo Recipe.txt para comprobar que se cumpliera
+// el principio de sustitucion LSP. Pudimos comprobar que al realizar nuestros cambios
+// el programa siguio funcionando de la misma forma que antes cumpliendolo.
+//---------------------------------------------------------------------------------
 using System;
 using System.IO;
 
